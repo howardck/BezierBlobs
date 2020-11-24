d24631300a1b5ce0bd5a2ad5030ea7300463d560
howardckatz <howardk@fatdog.com>
2020-11-24 07:14:40 -0800

    Initial Commit

---
+++ BezierBlobs/BezierBlobsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierBlobsApp.swift
+//  BezierBlobs
+//
+//  Created by Howard Katz on 2020-11-24.
+//
+
+import SwiftUI
+
+@main
+struct BezierBlobsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BezierBlobs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BezierBlobs
+//
+//  Created by Howard Katz on 2020-11-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
