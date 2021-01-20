3dc14d9808c2e17df9eb47ef38e20f6da55c62e7
howardckatz <howardk@fatdog.com>
2021-01-20 08:03:58 -0800

    minor

--- BezierBlobs/Model.swift
+++ BezierBlobs/Model.swift
@@ -164,19 +164,19 @@ class Model: ObservableObject { // init() { print("Model.init()") }
     func debugPrint(i: Int, theta: Double, vertex: CGPoint, normal: CGVector) {
 
         print( "[\((i).format(fspec: "2"))]  " +
-                "theta: {\((theta).format(fspec: "5.3"))}  {" +
-                "x: \((vertex.x).format(fspec: "7.2")), " +
-                "y: \((vertex.y).format(fspec: "7.2"))},  {" +
-                "dX: \((normal.dx).format(fspec: "5.3")), " +
-                "dY: \((normal.dy).format(fspec: "5.3"))}")
+                "theta: {\(theta.format(fspec: "5.3"))}  {" +
+                "x: \(vertex.x.format(fspec: "7.2")), " +
+                "y: \(vertex.y.format(fspec: "7.2"))},  {" +
+                "dX: \(normal.dx.format(fspec: "5.3")), " +
+                "dY: \(normal.dy.format(fspec: "5.3"))}")
         
 //        alternatively:
-//        let i_s = "\((i).format(fspec: "2"))"
-//        let theta_s = "\((theta).format(fspec: "5.3"))"
-//        let x_s = "\((vertex.x).format(fspec:" 7.2"))"
-//        let y_s = "\((vertex.y).format(fspec: "7.2"))"
-//        let dx_s = "\((normal.dx).format(fspec: "5.3"))"
-//        let dy_s = "\((normal.dy).format(fspec: "5.3"))"
+//        let i_s = "\(i.format(fspec: "2"))"
+//        let theta_s = "\(theta.format(fspec: "5.3"))"
+//        let x_s = "\(vertex.x.format(fspec:" 7.2"))"
+//        let y_s = "\(vertex.y.format(fspec: "7.2"))"
+//        let dx_s = "\(normal.dx.format(fspec: "5.3"))"
+//        let dy_s = "\(normal.dy.format(fspec: "5.3"))"
 //        print( "[\(i_s)] theta: {\(theta_s)} | {x: \(x_s), y: \(y_s)} | {dx: \(dx_s), dy: \(dy_s)}" )
     }
 
