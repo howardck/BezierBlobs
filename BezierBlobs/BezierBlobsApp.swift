f6ab9947cabfbe98dc7c3f6f17916e214a204a09
howardckatz <howardk@fatdog.com>
2021-01-21 06:54:44 -0800

    made TabViewTest slightly more complex; order of page appearance/disappearance still as per expected

--- BezierBlobs/BezierBlobsApp.swift
+++ BezierBlobs/BezierBlobsApp.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct BezierBlobsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-//            TabViewTest()
+            //ContentView()
+            TabViewTest()
         }
     }
 }
--- BezierBlobs/TabViewTest.swift
+++ BezierBlobs/TabViewTest.swift
@@ -7,27 +7,28 @@
 
 import SwiftUI
 
+struct SomeView: View {
+    let name: String
+    var body: some View {
+        Text(name)
+    }
+}
+
 struct TabViewTest: View {
     var body: some View {
         TabView {
             Text("Tab #1")
-                .onAppear{
-                    print("TAB PAGE #1 appearing")
-                }
-                .onDisappear{
-                    print("TAB PAGE #1 disappearing")
-                }
+//            SomeView(name: "I am Tab #1")
+                .onAppear{print("TAB PAGE #1 appearing")}
+                .onDisappear{print("TAB PAGE #1 disappearing")}
                 .tabItem {
                     Image(systemName: "1.square.fill")
                     Text("TAB 1")
                 }
             Text("Tab #2")
-                .onAppear{
-                    print("TAB PAGE #2 appearing")
-                }
-                .onDisappear{
-                    print("TAB PAGE #2 disappearing")
-                }
+//            SomeView(name: "I am Tab #2")
+                .onAppear{print("TAB PAGE #2 appearing")}
+                .onDisappear{print("TAB PAGE #2 disappearing")}
                 .tabItem {
                     Image(systemName: "2.square.fill")
                     Text("TAB 2")
