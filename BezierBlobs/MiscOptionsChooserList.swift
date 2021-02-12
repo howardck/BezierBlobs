a9eac45b8324f44f728fd7458dceaefaddd0adb7
howardckatz <howardk@fatdog.com>
2021-02-11 19:36:46 -0800

    MiscOptionsChooserList adds 2nd short list onscreen along w/ existing Layers one

---
+++ BezierBlobs/MiscOptionsChooserList.swift
@@ -0,0 +1,20 @@
+//
+//  MiscOptionsSelectionList.swift
+//  BezierBlobs
+//
+//  Created by Howard Katz on 2021-02-11.
+//
+
+import SwiftUI
+
+struct MiscOptionsChooserList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MiscOptionsSelectionList_Previews: PreviewProvider {
+    static var previews: some View {
+        MiscOptionsChooserList()
+    }
+}
