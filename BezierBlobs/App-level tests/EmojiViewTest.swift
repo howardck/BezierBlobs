42b09eb4f9b98ac0bf192965ca3a52368e933ac4
howardckatz <howardk@fatdog.com>
2021-02-16 01:18:46 -0800

    EmojiViewTest for Valentine's Day (BANG) + some other minor file renaming

---
+++ BezierBlobs/App-level tests/EmojiViewTest.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiViewTest.swift
+//  BezierBlobs
+//
+//  Created by Howard Katz on 2021-02-14.
+//
+
+import SwiftUI
+
+struct EmojiViewTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmojiViewTest_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiViewTest()
+    }
+}
