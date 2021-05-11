2aceee82aaf8a180c0fc813fd9c13d0675352861
howardckatz <howardk@fatdog.com>
2021-05-11 08:11:53 -0700

    starting to code to test EnumBasedListModel : ObservableObj

--- BezierBlobs/ContentView_TestAlternatives/EnumPerturbOptions_TEST.swift
+++ BezierBlobs/ContentView_TestAlternatives/EnumPerturbOptions_TEST.swift
@@ -7,11 +7,21 @@
 
 import SwiftUI
 
+class EnumBasedListModel : ObservableObject {
+    
+}
+
+
+
 struct EnumPerturbOptions_TEST: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.largeTitle)
             .background(Color.red)
             .foregroundColor(.orange)
+            .frame(width: 400, height: 200, alignment: .leading)
+            .background(Color.yellow)
+            .border(Color.blue)
             .onAppear {
                 print("EnumPerturbOptions_TEST().onAppear()")
             }
