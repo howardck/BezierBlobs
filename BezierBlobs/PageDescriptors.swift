a6d06c1e2549c03a8533367aa06769246f6f42a6
howardckatz <howardk@fatdog.com>
2021-06-17 10:45:31 -0700

    removed test version of enum-based PageDescriptors dictionary

--- BezierBlobs/PageDescriptors.swift
+++ BezierBlobs/PageDescriptors.swift
@@ -49,45 +49,6 @@ struct PageDescriptors {
                                 axisRelDeltas: (innerRange: -0.1..<0.2, outerRange: -0.3..<0.2))
 }
 
-/*
-    an alternative approach (not pursued) using the PageType enum as a dict key
- */
-
-//struct DictBasedPageDescriptors {
-//    
-//    static let descriptorsForPagesDictionary :
-//        
-//        [ PageType : (order: Double, numPoints: Int,
-//                      axisRelOffsets: (inner: CGFloat, baseCurve: CGFloat, outer: CGFloat),
-//                      axisRelDeltas : (innerRange: Range<CGFloat>, outerRange: Range<CGFloat>),
-//                      forceEqualAxes: Bool) ] =
-//        
-//        [ .circle:
-//            (order: 2.0, numPoints: 22,
-//             axisRelOffsets: (inner: 0.25, baseCurve: 0.5, outer: 0.7),
-//             axisRelDeltas: (innerRange: 0.1..<0.3, outerRange: -0.3..<0.3),
-//             forceEqualAxes: true),
-//          
-//          .classicSE:
-//            (order: 2.0, numPoints: 22,
-//             axisRelOffsets: (inner: 0.25, baseCurve: 0.5, outer: 0.7),
-//             axisRelDeltas: (innerRange: 0.1..<0.3, outerRange: -0.3..<0.3),
-//             forceEqualAxes: true),
-//          
-//          .deltaWing:
-//            (order: 2.0, numPoints: 22,
-//             axisRelOffsets: (inner: 0.25, baseCurve: 0.5, outer: 0.7),
-//             axisRelDeltas: (innerRange: 0.1..<0.3, outerRange: -0.3..<0.3),
-//             forceEqualAxes: true),
-//          
-//          .rorschach:
-//            (order: 2.0, numPoints: 22,
-//             axisRelOffsets: (inner: 0.25, baseCurve: 0.5, outer: 0.7),
-//             axisRelDeltas: (innerRange: 0.1..<0.3, outerRange: -0.3..<0.3),
-//             forceEqualAxes: true)
-//        ]
-//}
-
 struct PageDescriptors_TEST: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
